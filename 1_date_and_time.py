1472c297abfacef876b3e40de9eb295936ddf013
DavydovPA <magotov92@internet.ru>
2024-05-30 21:25:34 +0300

    init

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,33 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
+import locale
+locale.setlocale(locale.LC_TIME, 'ru_RU')
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    date_nw = datetime.now()
+    dt_nw = date_nw.strftime('%A %d %B %Y')
+
+    delta = timedelta(days = 1)
+    dt_yesterday = date_nw - delta
+    dt_ystd = dt_yesterday.strftime('%A %d %B %Y')
+
+    delta_2 = timedelta(days = 30)
+    dt_30 = date_nw - delta_2
+    dt_back = dt_30.strftime('%A %d %B %Y')
+
+    print(dt_ystd,dt_nw, dt_back, sep = '\n')
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    date_string = '01/01/20 12:10:03.234567'
+    date_dat = datetime.strptime(date_string, '%d/%m/%Y %H:%M:%S.%f')
+    print(date_dat)
+
+
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -10,13 +10,54 @@
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
 """
-
+import shutil
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+
+    with open('referat.txt', 'r', encoding='utf-8') as ref:
+
+        content = ref.read()
+        ref_2 = len(content)
+        print('Длинна строки:', ref_2)
+
+        ref_3 = len(content.split())
+        print(f"Количество слов: {ref_3}")
+
+    shutil.copyfile('referat.txt', 'referat2.txt')
+
+    with open('referat2.txt', 'r', encoding='utf-8') as f:
+        f_str = f.read()
+        f_str = f_str.replace('.', '!')
+
+    with open('referat2.txt', 'w', encoding='utf-8') as f:
+        f.write(f_str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
